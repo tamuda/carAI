b585ebf398234e5b71ed6c1343a09d6cb71671aa
Tamuda <tchimhan@u.rochester.edu>
2025-10-20 18:05:25 -0400

    Fix PWA icons: generate proper icons with dark background and update manifest

--- next-pwa.d.ts
+++ next-pwa.d.ts
@@ -1,6 +1,6 @@
-declare module 'next-pwa' {
-  import { NextConfig } from 'next';
-  
+declare module "next-pwa" {
+  import { NextConfig } from "next";
+
   interface PWAConfig {
     dest?: string;
     disable?: boolean;
@@ -10,7 +10,6 @@ declare module 'next-pwa' {
   }
 
   function withPWA(config: PWAConfig): (nextConfig: NextConfig) => NextConfig;
-  
+
   export default withPWA;
 }
-
