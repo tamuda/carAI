e308dfc537380b319c114738bef2250f5d1458ef
Tamuda <tchimhan@u.rochester.edu>
2025-10-20 17:59:20 -0400

    Add TypeScript declarations for next-pwa

---
+++ next-pwa.d.ts
@@ -0,0 +1,16 @@
+declare module 'next-pwa' {
+  import { NextConfig } from 'next';
+  
+  interface PWAConfig {
+    dest?: string;
+    disable?: boolean;
+    register?: boolean;
+    skipWaiting?: boolean;
+    [key: string]: any;
+  }
+
+  function withPWA(config: PWAConfig): (nextConfig: NextConfig) => NextConfig;
+  
+  export default withPWA;
+}
+
